5e1004db064c9286e9baac321e75e6864530ddd7
Oil-Limk <kmilox.urzua.g@gmail.com>
2025-12-11 02:25:24 -0300

    d05.1

---
+++ d05.1.py
@@ -0,0 +1,38 @@
+import sys
+
+def str2inttup(rr:str):
+  i, j = rr.split('-')
+  return int(i), int(j)
+
+lines = open(sys.argv[1]).read().strip()
+rranges, ringredients = lines.split('\n\n')
+ranges = list(map(str2inttup, rranges.strip().split('\n')))
+ingredients = list(map(int, ringredients.strip().split('\n')))
+
+a = 0
+
+while a < len(ranges):
+  ia, ja = ranges[a]
+  b = 0
+  merged = False
+  while b < len(ranges):
+    if b != a:
+      ib, jb = ranges[b]
+      if ib <= ia <= jb or ib <= ja <= jb:
+        ranges[b] = (min(ia, ib), max(ja, jb))
+        ranges.pop(a)
+        merged = True
+        break
+    b += 1
+  if not merged:
+    a += 1
+
+result = 0
+
+for ing in ingredients:
+  for i, j in ranges:
+    if i <= ing <= j:
+      result += 1
+      break
+
+print(result)
