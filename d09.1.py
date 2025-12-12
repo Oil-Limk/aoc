7975635d62a5dcb1d6c3e2c07e10ccf350791232
Oil-Limk <kmilox.urzua.g@gmail.com>
2025-12-11 21:54:26 -0300

    d09.1

---
+++ d09.1.py
@@ -0,0 +1,19 @@
+import sys
+
+lines = open(sys.argv[1]).read().strip()
+sqrs = [[int(x) for x in line.split(',')] for line in lines.split('\n')]
+m = len(sqrs)
+
+result = 0
+i = 0
+while i < m:
+  j = i + 1
+  while j < m:
+    xi, yi = sqrs[i]
+    xj, yj = sqrs[j]
+    a = abs((xi - xj + 1) * (yi - yj + 1))
+    if a > result: result = a
+    j += 1
+  i += 1
+
+print(result)
