43ef54da4f84db4ff28ba75815e42194ed971913
Oil-Limk <kmilox.urzua.g@gmail.com>
2026-07-21 23:52:19 -0400

    d25.1/2015

---
+++ 2015/d25.1.py
@@ -0,0 +1,10 @@
+import sys
+message = open(sys.argv[1]).readline().strip().split(' ')
+col = int(message[-1][:-1]) - 1
+both = col + int(message[-3][:-1]) - 1
+exponent = col + (both * (both + 1)) // 2
+start = 20151125
+factor = 252533
+module = 33554393
+result = start * pow(factor, exponent, module) % module
+print(result)
