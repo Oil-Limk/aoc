c40b51ce9f7d1456ad520e02e4be03940f4dcb82
Oil-Limk <kmilox.urzua.g@gmail.com>
2026-07-21 23:51:39 -0400

    d10/2015

---
+++ 2015/d10.2.py
@@ -0,0 +1,24 @@
+import sys
+
+seq = open(sys.argv[1]).readline().strip()
+
+def next_look_and_say(seq):
+  next_seq = ""
+  n = seq[0]
+  c = 1
+  for s in seq[1:]:
+    if s == n:
+      c += 1
+    else:
+      next_seq += f"{c}{n}"
+      n = s
+      c = 1
+  next_seq += f"{c}{n}"
+  return next_seq
+
+for _ in range(50):
+  seq = next_look_and_say(seq)
+
+result = len(seq)
+
+print(result)
