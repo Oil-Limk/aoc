ede3fea70f636522584eedca41a092d906e95199
imanoisyeater <imanoisyeater@proton.me>
2026-08-22 13:23:50 -0400

    d19/2015

---
+++ 2015/d19.py
@@ -0,0 +1,20 @@
+import sys
+import re
+
+with open(sys.argv[1]) as f:
+    _, molecule = f.read().strip().split("\n\n")
+
+molecule = molecule.replace("Rn", "(")
+molecule = molecule.replace("Ar", ")")
+molecule = molecule.replace("Y", ",")
+molecule = re.sub("[a-z]","",molecule)
+molecule = re.sub("[A-Z]","X",molecule)
+
+n = 0
+while len(molecule) > 1:
+    for p in ["X(X,X,X)", "X(X,X)", "X(X)", "XX"]:
+        if p in molecule:
+            molecule = molecule.replace(p,"X", 1)
+            n += 1
+print(n, molecule)
+
